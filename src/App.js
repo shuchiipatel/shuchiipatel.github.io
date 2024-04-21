1ca8eb820b2e78c7e9a1361216890a464973f23f
Mithil Parmar <mithilparmar09@gmail.com>
2024-04-21 01:48:09 -0700

    added cursor style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom'
+import AnimatedCursor from "react-animated-cursor"
 import Layout from './components/Layout'
 import Home from './components/Home';
 import About from './components/About';
@@ -9,6 +10,21 @@ import Project from './components/Project';
 function App() {
   return (
     <>
+    <AnimatedCursor
+      color='198, 222, 65'
+      innerSize={5}
+      outerSize={25}
+      innerScale={1}
+      outerScale={2}
+      outerAlpha={0}
+      hasBlendMode={true}
+      innerStyle={{
+        backgroundColor: '#C6DE41'
+      }}
+      outerStyle={{
+        border: '1px solid #C6DE41',
+      }}
+    />
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />

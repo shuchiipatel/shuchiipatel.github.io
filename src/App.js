625558fd5f29fdeaf12b9c3e8fd356fddc8984e2
Mithil Parmar <mithilparmar09@gmail.com>
2024-04-21 01:55:09 -0700

    fixed cursor showing up on mobile devices

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom'
 import AnimatedCursor from "react-animated-cursor"
+import { isMobile } from 'react-device-detect'
 import Layout from './components/Layout'
 import Home from './components/Home';
 import About from './components/About';
@@ -8,23 +9,28 @@ import Contact from './components/Contact';
 import Project from './components/Project';
 
 function App() {
+
+  const isMobileDevice = isMobile;
+
   return (
     <>
-    <AnimatedCursor
-      color='198, 222, 65'
-      innerSize={5}
-      outerSize={25}
-      innerScale={1}
-      outerScale={2}
-      outerAlpha={0}
-      hasBlendMode={true}
-      innerStyle={{
-        backgroundColor: '#C6DE41'
-      }}
-      outerStyle={{
-        border: '1px solid #C6DE41',
-      }}
-    />
+    {!isMobileDevice && (
+      <AnimatedCursor
+        color='198, 222, 65'
+        innerSize={5}
+        outerSize={25}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: '#C6DE41'
+        }}
+        outerStyle={{
+          border: '1px solid #C6DE41',
+        }}
+      />
+    )}
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
